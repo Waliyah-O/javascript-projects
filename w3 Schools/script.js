aa6efa0eb4080c6db86e2e932c81709dedc6428c
Waliyah <129737697+Waliyah-O@users.noreply.github.com>
2024-05-29 22:06:58 +0100

    Add files via upload

---
+++ w3 Schools/script.js
@@ -0,0 +1,12 @@
+/*****************DOM MANIPULATION ***********/
+//  function myFunction() {
+//     document.getElementById("demo").innerHTML = "Hello Javascript"
+//     // console.log("WHYYYYYYYYYyyyyyyyy");
+//  }
+//  document.getElementById("demo").style.fontSize = "35px";
+
+let a, b, c;
+a = "hege";
+b = " Hedge";
+c = a + b;
+console.log(c);
